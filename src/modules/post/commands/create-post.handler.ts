5b29ad04b504b4e97435634df022b28de5726906
bahaa-alden <samerabood195@gmail.com>
2025-01-05 04:25:34 +0300

    fix: translations

--- src/modules/post/commands/create-post.handler.ts
+++ src/modules/post/commands/create-post.handler.ts
@@ -3,6 +3,8 @@ import { CommandHandler } from '@nestjs/cqrs';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import { CreateTranslationDto } from '../../../common/dto/create-translation.dto.ts';
+import { CreatePostDto } from '../dtos/create-post.dto.ts';
 import { PostEntity } from '../post.entity.ts';
 import { PostTranslationEntity } from '../post-translation.entity.ts';
 import { CreatePostCommand } from './create-post.command.ts';
@@ -21,29 +23,34 @@ export class CreatePostHandler
   async execute(command: CreatePostCommand) {
     const { userId, createPostDto } = command;
     const postEntity = this.postRepository.create({ userId });
-    const translations: PostTranslationEntity[] = [];
+    const translations = this.createTranslations(createPostDto, postEntity.id);
 
     await this.postRepository.save(postEntity);
-
-    // FIXME: Create generic function for translation creation
-    for (const createTranslationDto of createPostDto.title) {
-      const languageCode = createTranslationDto.languageCode;
-      const translationEntity = this.postTranslationRepository.create({
-        postId: postEntity.id,
-        languageCode,
-        title: createTranslationDto.text,
-        description: createPostDto.description.find(
-          (desc) => desc.languageCode === languageCode,
-        )!.text,
-      });
-
-      translations.push(translationEntity);
-    }
 
     await this.postTranslationRepository.save(translations);
 
     postEntity.translations = translations;
 
     return postEntity;
+  }
+
+  private createTranslations(
+    createPostDto: CreatePostDto,
+    postId: string,
+  ): PostTranslationEntity[] {
+    return createPostDto.title.map(
+      (createTranslationDto: CreateTranslationDto) => {
+        const languageCode = createTranslationDto.languageCode;
+
+        return this.postTranslationRepository.create({
+          postId,
+          languageCode,
+          title: createTranslationDto.text,
+          description: createPostDto.description.find(
+            (desc) => desc.languageCode === languageCode,
+          )!.text,
+        });
+      },
+    );
   }
 }
